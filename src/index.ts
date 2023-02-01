f5fa398d0ff04834553cdaf72d633020df5dcf03
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-02-01 04:32:19 -0500

    added dubugger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import * as dotenv from 'dotenv';
 
-import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import morgan from 'morgan';
-import { nanoid } from 'nanoid';
 
+dotenv.config();
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan('dev')); //middleware logger
 
 app.get('/', async (req, res) => {
-  res.status(200).send('Test');
+  const ip = req.ip;
+  //   debugger;
+  res.status(200).send(`
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Node Server: Welcome</title>
+   </head>
+<body>
+<style>  *{margin:0; padding:0; box-sizing:border-box; } </style>
+<h1 style="text-align:center;>WELCOME TO THE GULAG</h1>
+<p style="text-align:center;">Server Started at ${ip}:${PORT}</p>
+   </body>
+</html>`);
 });
 
-const PORT = parseInt(process.env.PORT as string, 10) || 8081;
+const PORT = Number(process.env.PORT || 300);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server started at ${PORT}`);
